36fe03a9689b4b866d1069819b8753d794266e32
ferdinandl007 <ferdinandloesch@me.com>
2018-11-24 14:49:22 +0000

    Initial Commit

---
+++ FindTests/FindTests.swift
@@ -0,0 +1,34 @@
+//
+//  FindTests.swift
+//  FindTests
+//
+//  Created by Ferdinand Lösch on 24/11/2018.
+//  Copyright © 2018 Ferdinand Lösch. All rights reserved.
+//
+
+import XCTest
+@testable import Find
+
+class FindTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
